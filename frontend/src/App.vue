4b22080fbfb599c4794be474b073bbc1396a546b
Max Szafarski <maksymilian.szafarski@vocale.app>
2022-09-29 16:53:41 +0200

    fix App.vue so it is not losing data on refresh

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,50 +9,97 @@
 </template>
 
 <script>
-import axios from 'axios'
-import Header from './components/Header.vue'
-import Footer from './components/Footer.vue'
+import axios from "axios";
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 
 export default {
   components: {
     Header,
-    Footer
+    Footer,
   },
   beforeCreate() {
-    this.$store.commit('initStore')
+    this.$store.commit("initStore");
 
-    if (this.$store.state.id) {
-      axios
-        .get("/api/v1/token/users/", this.$store.state.id)
-        .then(response => {
-          const user = response.data.user
-
-          this.$store.commit('setUser', user)
-        })
-        .catch(error => {
-          console.log(error)
-        })
-    } else {
-      this.$store.commit('removeUser')
-    }
-
-    const token = this.$store.state.token
+    const token = this.$store.state.token;
 
     if (token) {
-      axios.defaults.headers.common['Authorization'] = 'Token ' + token
+      axios.defaults.headers.common["Authorization"] = "Token " + token;
     } else {
-      axios.defaults.headers.common['Authorization'] = ''
+      axios.defaults.headers.common["Authorization"] = "";
     }
+
+    if (localStorage.getItem("id")) {
+      axios
+        .get("/api/v1/users/me")
+        .then((response) => {
+          localStorage.setItem("id", response.data.id);
+
+          axios
+            .get(`/api/v1/profile/${localStorage.getItem("id")}`)
+            .then((response2) => {
+              const user = {
+                id: response.data.id,
+                email: response.data.email,
+                favourite_genres: response2.data.favourite_genres,
+                favourite_cinemas: response2.data.favourite_cinemas,
+              };
+
+              this.$store.commit("setUser", user);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      this.$store.commit("removeUser");
+    }
+
+    axios
+      .get("/api/v1/buildings-list")
+      .then((response) => {
+        let buildings = [];
+        response.data.forEach((element) => {
+          buildings.push(element.name);
+        });
+        this.$store.commit("setBuildings", buildings);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    axios
+      .get("/api/v1/genres-list")
+      .then((response) => {
+        let genres = [];
+        response.data.forEach((element) => {
+          genres.push(element.name);
+        });
+        this.$store.commit("setGenres", genres);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   },
-}
+};
 </script>
 
 <style lang="scss">
+@import "@/assets/main.scss";
+
+html {
+  overflow-y: auto !important;
+}
+
 #app {
   min-height: 100vh;
   margin: 0;
   display: grid;
   grid-template-rows: auto 1fr auto;
+  background-color: $grey-light;
 }
 
 .body {
@@ -60,5 +107,4 @@ export default {
   flex-direction: column;
   align-items: center;
 }
-
 </style>
