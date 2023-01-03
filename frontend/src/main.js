857b8555088155b7c3cb5022804b12b549e76a34
Max Szafarski <maksymilian.szafarski@vocale.app>
2023-01-03 18:17:39 +0100

    refactor and dockerize configuration

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,5 +7,7 @@ import axios from 'axios'
 require('@/assets/main.scss');
 
 axios.defaults.baseURL = 'http://127.0.0.1:8000'
+axios.defaults.xsrfCookieName = 'csrftoken'
+axios.defaults.xsrfHeaderName = 'X-CSRFToken'
 
 createApp(App).use(store).use(router, axios).mount('#app')
