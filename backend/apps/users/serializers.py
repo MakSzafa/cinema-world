4a0c67bca8c803893965cb2272655cd33b3a3d4a
Max Szafarski <maksymilian.szafarski@vocale.app>
2023-01-10 20:19:45 +0100

    customize token obtaining

--- backend/apps/users/serializers.py
+++ backend/apps/users/serializers.py
@@ -1,17 +1,21 @@
 from rest_framework import serializers
-
-from django.conf import settings
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.settings import api_settings
+from django.contrib.auth.models import update_last_login
 
 from apps.users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
-    registered_at = serializers.DateTimeField(format='%H:%M %d.%m.%Y', read_only=True)
-    url = serializers.HyperlinkedIdentityField(view_name='user-detail', lookup_field='pk')
+    registered_at = serializers.DateTimeField(
+        format='%H:%M %d.%m.%Y', read_only=True)
+    url = serializers.HyperlinkedIdentityField(
+        view_name='user-detail', lookup_field='pk')
 
     class Meta:
         model = User
-        fields = ['id', 'url', 'email', 'favourite_genres', 'favourite_cinemas', 'registered_at']
+        fields = ['id', 'url', 'email', 'favourite_genres',
+                  'favourite_cinemas', 'registered_at']
 
 
 class UserWriteSerializer(serializers.ModelSerializer):
@@ -27,8 +31,26 @@ class UserPasswordResetSerializer(serializers.ModelSerializer):
         model = User
         fields = ['email']
 
+
 class UserPasswordResetChangeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
         fields = ['email', 'password', 'password_reset_token']
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
+    def validate(self, attrs):
+        data = super().validate(attrs)
+
+        refresh = self.get_token(self.user)
+
+        data['refresh'] = str(refresh)
+        data['access'] = str(refresh.access_token)
+        data['id'] = self.user.id
+
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+
+        return data
--- backend/config/urls.py
+++ backend/config/urls.py
@@ -4,9 +4,8 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from apps.users.views import UserViewSet
+from apps.users.views import UserViewSet, MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -19,7 +18,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api/', include('apps.cinema.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
