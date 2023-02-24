191acb1d350e25403e74e9f2bbc1e783a52bfd5e
Max Szafarski <makszafa@gmail.com>
2023-02-24 16:45:56 +0100

    change axios baseURL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import axios from "axios";
 
 require("@/assets/main.scss");
 
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+axios.defaults.baseURL = "http://filmarket.makszafa.pl";
 axios.defaults.xsrfCookieName = "csrftoken";
 axios.defaults.xsrfHeaderName = "X-CSRFToken";
 
