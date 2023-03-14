476f8806869624eca7170fbcfa6dde92d2582bb2
Max Szafarski <makszafa@gmail.com>
2023-03-14 17:05:22 +0100

    add "/" to movie details endpoint

--- backend/apps/cinema/urls.py
+++ backend/apps/cinema/urls.py
@@ -4,7 +4,7 @@ from apps.cinema import views
 
 urlpatterns = [
     path('movies-list/', views.MoviesList.as_view()),
-    path('movie-details/<int:pk>', views.MovieDetails.as_view()),
+    path('movie-details/<int:pk>/', views.MovieDetails.as_view()),
     path('hottest-movies/', views.HottestMoviesList.as_view()),
     path('buildings-list/', views.BuildingsList.as_view()),
     path('genres-list/', views.GenresList.as_view()),
